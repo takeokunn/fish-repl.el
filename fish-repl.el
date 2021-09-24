eac14558c330f1d411553dac07a3220c1dab08d2
takeokunn <bararararatty@gmail.com>
2021-09-24 17:00:42 +0900

    Add LISENCE

---
+++ fish-repl.el
@@ -0,0 +1,32 @@
+;;; fish-repl.el --- summary -*- lexical-binding: t -*-
+
+;; Author: obara takeo
+;; Maintainer: obara takeo
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: https://github.com/takeokunn/fish-repl.el
+;; Keywords: fish
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(message "Hello World!")
+
+(provide 'fish-repl)
+
+;;; fish-repl.el ends here
